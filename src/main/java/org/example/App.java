18e19f2589b772626a073e324d63d69e353d2d98
Mohd Ali <khanali6861@gmail.com>
2025-07-29 17:09:52 +0530

    remove world

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from harvey Specter " );
+        System.out.println( "Hello from harvey Specter" );
     }
 }

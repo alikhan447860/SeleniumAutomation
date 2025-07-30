f0e14dd6ab797a397616de1129b886e0ddfa7546
Mohd Ali <khanali6861@gmail.com>
2025-07-30 14:21:01 +0530

    Test pipeline trigger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "from suits  harvey Specter" );
-        System.out.println("from suits mike ross");
+        System.out.println("from suits Mike Ross");
     }
 }
